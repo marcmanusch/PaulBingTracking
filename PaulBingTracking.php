a99104ac069c34d333e163116ec74f8925ac3d2f
marcmanusch <31078285+marcmanusch@users.noreply.github.com>
2018-01-28 12:47:38 +0100

    Update PaulBingTracking.php

--- PaulBingTracking.php
+++ PaulBingTracking.php
@@ -1,47 +1,18 @@
 <?php
 namespace PaulBingTracking;
-
-use Enlight_Controller_ActionEventArgs;
-use Shopware\Components\Plugin;
 use Shopware\Components\Plugin\Context\InstallContext;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Shopware\Components\Plugin;
 
-class PaulBingTracking extends Plugin {
-
-	public static function getSubscribedEvents() {
-		return [
-			'Enlight_Controller_Action_PreDispatch_Frontend' => 'onPostDispatch',
-			'Enlight_Controller_Action_PreDispatch_Widgets' => 'onPostDispatch',
-		];
-	}
-
-	public function install(InstallContext $context) {
-		$context->scheduleClearCache(InstallContext::CACHE_LIST_FRONTEND);
-	}
-
-	/**
-	 * Event listener method
-	 *
-	 * @param Enlight_Controller_ActionEventArgs $args
-	 */
-	public function onPostDispatch( Enlight_Controller_ActionEventArgs $args ) {
-		if (!Shopware()->Config()->getByNamespace('PaulBingTracking', 'show')) {
-			return;
-		}
-
-		$request = $args->getSubject()->Request();
-		$view    = $args->getSubject()->View();
-
-		$view->addTemplateDir( __DIR__ . '/Resources/Views/' );
-
-		if ( $request->isXmlHttpRequest() ) {
-			return;
-		}
-
-		if ( empty(Shopware()->Config()->getByNamespace('PaulBingTracking', 'UETID') ) ) {
-			return;
-		}
-
-		// Set template vars.
-		$view->paulBingTrackingUETID = Shopware()->Config()->getByNamespace('PaulBingTracking', 'UETID');
-	}
+class PaulBingTracking extends Plugin
+{
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->setParameter('paul_bing_tracking.plugin_dir', $this->getPath());
+        parent::build($container);
+    }
 }
+?>
