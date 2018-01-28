764187bcf6bd19c8438504f0319413212056d78d
marcmanusch <31078285+marcmanusch@users.noreply.github.com>
2018-01-28 12:42:59 +0100

    Create Frontend.php

---
+++ Subscriber/Frontend.php
@@ -0,0 +1,48 @@
+<?php
+namespace PaulBingTracking\Subscriber;
+use Enlight\Event\SubscriberInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class Frontend implements SubscriberInterface
+{
+    /** @var  ContainerInterface */
+    private $container;
+    /**
+     * Frontend contructor.
+     * @param ContainerInterface $container
+     **/
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'Enlight_Controller_Action_PostDispatchSecure_Frontend' => 'onFrontendPostDispatch',
+        ];
+    }
+    /**
+     * @param \Enlight_Event_EventArgs $args
+     */
+    public function onFrontendPostDispatch(\Enlight_Event_EventArgs $args)
+    {
+        
+        $config = $this->container->get('shopware.plugin.config_reader')->getByPluginName('PaulBingTracking', $shop);
+        
+        /** @var $controller \Enlight_Controller_Action */
+        $controller = $args->getSubject();
+        $view = $controller->View();
+        $view->addTemplateDir($this->container->getParameter('paul_bing_tracking.plugin_dir') . '/Resources/Views');
+        
+        // get plugin settings
+        $paulPlentyFreeActive = $config['paulPlentyFreeActive'];
+        $paulPlentyFreeNr = $config['paulPlentyFreeNr'];
+	    
+        // aggign to frontend
+        $view->assign('paulPlentyFreeActive', $paulPlentyFreeActive);
+        $view->assign('paulPlentyFreeNr', $paulPlentyFreeNr);
+    }
+}
